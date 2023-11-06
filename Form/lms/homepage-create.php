01f810f071d371dc330f197fd21125f14a24bac8
andreibrosmaster <111739317+andreibrosmaster@users.noreply.github.com>
2023-11-05 19:06:46 -0800

    Add files via upload

--- Form/lms/homepage-create.php
+++ Form/lms/homepage-create.php
@@ -1,45 +1,35 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == 'POST') {
-    if (isset($_POST['edit'])) {
-        $edited_textarea = $_POST['edited-textarea'];
-        $edited_video = $_FILES['edited-video'];
+    if (isset($_POST['create'])) {
+        // Registration code
+        $course_name = $_POST['textarea'];
 
-        // Connect to the database (replace with your database connection code)
-        $conn = mysqli_connect("hostname", "username", "password", "database_name");
+       
+        $video = $_FILES['video']['name']; // Get the filename of the uploaded video
+        $video_tmp = $_FILES['video']['tmp_name']; // Get the temporary location of the uploaded video
+        $video_path = 'assets/images/' . $video; // Set the desired path to store the uploaded video
 
-        if (!$conn) {
-            die("Connection failed: " . mysqli_connect_error());
-        }
+        // Establish Connection
+        require_once('connection.php');
 
-        // Handle video upload if a new video is selected
-        if ($edited_video['error'] === 0) {
-            $edited_video_name = $edited_video['name'];
-            $edited_video_tmp = $edited_video['tmp_name'];
-            $edited_video_path = 'assets/images/' . $edited_video_name;
-
-            // Update the homepage content with the edited textarea and new video
-            $sql = "UPDATE homepage SET textarea = '$edited_textarea', video = '$edited_video_path' WHERE id = 1";
-
-            if (mysqli_query($conn, $sql)) {
-                // Move the uploaded video to the desired location
-                move_uploaded_file($edited_video_tmp, $edited_video_path);
-
-                echo '<script>window.location = "homepage-db.php";</script>';
-            } else {
-                echo "Error updating homepage content: " . mysqli_error($conn);
-            }
+        // Check the connection
+        if (mysqli_connect_errno()) {
+            echo "Failed to connect!";
+            exit();
         } else {
-            // Update the homepage content with the edited textarea (no new video)
-            $sql = "UPDATE homepage SET textarea = '$edited_textarea' WHERE id = 1";
+            // Insert user data into the database
+            $sql = "INSERT INTO homepage (textarea, video) VALUES ('$course_name', '$video_path')";
+            $result = mysqli_query($conn, $sql);
+            if ($result) {
+                // Move the uploaded video to the desired location
+                move_uploaded_file($video_tmp, $video_path);
 
-            if (mysqli_query($conn, $sql)) {
                 echo '<script>window.location = "homepage-db.php";</script>';
+                exit();
             } else {
-                echo "Error updating homepage content: " . mysqli_error($conn);
+                die(mysqli_error($conn));
             }
         }
-
-        mysqli_close($conn);
-    }
+    } 
 }
 ?>
